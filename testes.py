14f848cba20e24a746adc0af33ac98cb8318eba6
Sérgio Verton <sverton611@gmail.com>
2021-05-27 12:59:38 -0300

    Exercícios terminados.

---
+++ testes.py
@@ -0,0 +1,25 @@
+n1 = int(input())
+n2 = int(input())
+
+if n1 + n2 == 0:
+    print(f'0 - PROXICITY')
+elif n1+ n2 == 1:
+    print(f'1 - P.Y.N.G')
+elif n1 + n2 == 2:
+    print(f'2 - DNSUEY')
+elif n1 + n2 == 3:
+    print(f'3 - SERVERS')
+elif n1 + n2 == 4:
+    print(f'4 - HOST')
+elif n1 + n2 == 5:
+    print(f'5 - CRIPTONIZE')
+elif n1 + n2 == 6:
+    print(f'6 - OFFLINE DAY')
+elif n1 + n2 == 7:
+    print(f'7 - SALT')
+elif n1 + n2 == 8:
+    print(f'8 - ANSWER')
+elif n1 + n2 == 9:
+    print(f'9 - RAR')
+elif n1 + n2 == 10:
+    print(f'10 - WIFI ANTENNAS')
